fbd397d2ed25b4618bcb406a183fe972fdf41b0b
Alexander Krupenko <alexanderkrupenko@gmail.com>
2020-05-14 23:01:31 +0300

    1st commit

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+import java.util.Arrays;
+import java.util.List;
+import java.util.Set;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.function.Function;
+import java.util.function.Predicate;
+import java.util.stream.Collectors;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+    }
+
+}
